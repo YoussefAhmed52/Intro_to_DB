96b795bbb8831df5e40ffd0db33324e2a8e91616
YoussefAhmed52 <yousef3012001@gmail.com>
2025-06-27 08:06:22 -0400

    task2

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,25 @@
 USE alx_book_store;
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+)
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+)
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE 
+)
