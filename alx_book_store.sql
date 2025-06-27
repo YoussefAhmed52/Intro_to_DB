582ad6f8e842b6b5b6604ceb097e33856f89a510
YoussefAhmed52 <yousef3012001@gmail.com>
2025-06-27 07:17:10 -0400

    SQL

---
+++ alx_book_store.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY, 
+    title VARCHAR(130), 
+    author_id FOREIGN KEY REFERENCES Authors , 
+    price DOUBLE, 
+    publication_date DATE
+    )
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+)
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id FOREIGN KEY REFERENCES Customers,
+    order_date DATE
+)
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id FOREIGN KEY REFERENCES Orders,
+    book_id FOREIGN KEY REFERENCES Books,
+    quantity DOUBLE 
+)
